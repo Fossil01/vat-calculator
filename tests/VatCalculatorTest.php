2957316088eaef7f8f3f286b9433e6115c382639
Fossil <Fossil01@users.noreply.github.com>
2019-08-07 14:10:09 +0200

    Update VatCalculatorTest.php

--- tests/VatCalculatorTest.php
+++ tests/VatCalculatorTest.php
@@ -484,7 +484,7 @@ class VatCalculatorTest extends PHPUnit
 
     public function testCanResolveIPToCountry()
     {
-        self::$file_get_contents_result = '{"countryCode":"US","countryCode3":"USA","countryName":"United States","countryEmoji":"🇺🇸"}';
+        self::$file_get_contents_result = '{"ip":"23.226.132.236","type":"ipv4","continent_code":"NA","continent_name":"North America","country_code":"US","country_name":"United States","region_code":"NJ","region_name":"New Jersey","city":"Secaucus","zip":"07094","latitude":40.7876,"longitude":-74.06,"location":{"geoname_id":null,"capital":"Washington D.C.","languages":[{"code":"en","name":"English","native":"English"}],"country_flag":"http:\/\/assets.ipapi.com\/flags\/us.svg","country_flag_emoji":"\ud83c\uddfa\ud83c\uddf8","country_flag_emoji_unicode":"U+1F1FA U+1F1F8","calling_code":"1","is_eu":false}}';
         $vatCalculator = new VatCalculator();
         $country = $vatCalculator->getIPBasedCountry();
         $this->assertEquals('US', $country);
